8d5ba336eee955f40b0b7643b319d70959d273e0
mohsenshakiba <work.shakiba@gmail.com>
2019-08-15 11:07:00 +0430

    added log

--- ElasticSearchExample/Repositories/CitiesRepository.cs
+++ ElasticSearchExample/Repositories/CitiesRepository.cs
@@ -51,7 +51,7 @@ namespace ElasticSearchExample.Repositories
                 var options = new SeparatedValueOptions() { IsFirstRecordSchema = true };
                 var cites = mapper.Read(reader, options).ToList();
                 var result = await client.IndexManyAsync(cites, IndexName);
-                Console.WriteLine($"added {cites.Count} documents with {result.IsValid}");
+                Console.WriteLine($"added {cites.Count} documents with {result.IsValid} error: {result.ItemsWithErrors.FirstOrDefault()?.Error}");
             }
         }
     }
